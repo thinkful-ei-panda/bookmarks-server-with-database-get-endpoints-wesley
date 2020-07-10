4996208b29a3e6b773b2ee394b968d23102747fc
Wesley Rou <wesleyrou@gmail.com>
2020-07-10 11:37:19 -0700

    added GET bookmarks path

---
+++ src/BOOKMARKS.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id:1,
+    title:'Bookmark1',
+    rating:'5',
+    url:'google.com',
+    description:'search engine'
+  },
+  {
+    id:2,
+    title:'Bookmark2',
+    rating:'5',
+    url:'google.com',
+    description:'search engine'
+  },
+  {
+    id:3,
+    title:'Bookmark3',
+    rating:'5',
+    url:'google.com',
+    description:'search engine'
+  },
+  {
+    id:4,
+    title:'Bookmark4',
+    rating:'5',
+    url:'google.com',
+    description:'search engine'
+  },
+];
---
+++ src/app.js
@@ -0,0 +1,53 @@
+require ('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const {NODE_ENV} = require('./config');
+const logger = require ('./logger');
+const bookmarks = require ('./BOOKMARKS');
+
+const app = express();
+
+const morgOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morgOption));
+app.use(cors());
+app.use(helmet());
+
+app.use(function validateApiToken(req,res,next){
+  const BearerToken=req.get('Authorization');
+  const API_TOKEN = process.env.API_TOKEN;
+  if(!API_TOKEN || API_TOKEN!==BearerToken.split(' ')[1]){
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    res.status(401).json({error:'Unauthorized user'});
+  }
+  next();
+});
+
+app.get('/bookmarks', (req,res) => {
+  let response=bookmarks;  
+  res.status(200).send(response);
+});
+
+app.get('/bookmarks/:id', (req,res) => {
+
+});
+
+
+
+app.use(function errorHandler(error,req,res,next){ //eslint-disable-line
+  let response;
+  if(NODE_ENV === 'production') {
+    response = {error: {message: 'server error'}};
+  }else{
+    console.log(error);
+    response = {message: error.message, error};
+  }
+  res.status(400).json(response);
+});
+
+module.exports = app;
